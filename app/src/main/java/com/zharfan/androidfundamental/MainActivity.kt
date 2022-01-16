4e925d333cee92a02b8ea38e7048bc8d56408407
zharfanwafiq <zharfanww@gmail.com>
2022-01-16 10:59:19 +0700

    adding debug and logging

--- app/src/main/java/com/zharfan/androidfundamental/MainActivity.kt
+++ app/src/main/java/com/zharfan/androidfundamental/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import com.zharfan.androidfundamental.data.Person
 import com.zharfan.androidfundamental.databinding.ActivityMainBinding
+import com.zharfan.androidfundamental.debug.DebugActivity
 
 import com.zharfan.androidfundamental.intent.MoveActivity
 import com.zharfan.androidfundamental.intent.MoveActivityWithData
@@ -17,14 +18,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setActionButton()
+    }
+
+    private fun setActionButton() {
+        binding.apply {
+            btnIntent.setOnClickListener {
+                startActivity(Intent(this@MainActivity, DebugActivity ::class.java))
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/zharfan/androidfundamental/debug/DebugActivity.kt
@@ -0,0 +1,74 @@
+package com.zharfan.androidfundamental.debug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.zharfan.androidfundamental.R
+import java.lang.StringBuilder
+
+
+
+class DebugActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText : TextView
+
+    private var names = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debug)
+
+        showActionBar()
+        setInitView()
+        setData()
+    }
+
+    private fun setData() {
+        names.add("Zharfan")
+        names.add("Wafiq")
+        names.add(("ganteng"))
+    }
+
+    private fun setInitView() {
+        tvText = findViewById(R.id.tvText)
+        btnSetValue = findViewById(R.id.btnSetValue)
+        btnSetValue.setOnClickListener(this)
+    }
+
+    private fun showActionBar() {
+        supportActionBar?.apply {
+            title = "Debugging & Logging"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home ->onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btnSetValue -> {
+                Log.d("DebugActivity",names.toString())
+                val name = StringBuilder()
+                for (i in 0..3){
+                    name.append(names[i]).append("\n")
+                }
+                tvText.text = name.toString()
+            }
+        }
+
+    }
+
+
+}
