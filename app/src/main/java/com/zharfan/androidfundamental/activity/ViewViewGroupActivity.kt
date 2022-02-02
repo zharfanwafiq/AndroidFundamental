95d8232325646d1ca98d270d60b162eea8e4b124
zharfanwafiq <zharfanww@gmail.com>
2022-02-02 18:24:27 +0700

    adding view viewgroup.

---
+++ app/src/main/java/com/zharfan/androidfundamental/activity/ViewViewGroupActivity.kt
@@ -0,0 +1,15 @@
+package com.zharfan.androidfundamental.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zharfan.androidfundamental.databinding.ActivityViewViewGroupBinding
+
+class ViewViewGroupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewViewGroupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityViewViewGroupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
