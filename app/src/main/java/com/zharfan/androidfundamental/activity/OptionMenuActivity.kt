8ce17e8f61a27576cd37c3033213210ab2171c8c
zharfanwafiq <zharfanww@gmail.com>
2022-03-20 06:26:21 +0700

    add search view

--- app/src/main/java/com/zharfan/androidfundamental/activity/OptionMenuActivity.kt
+++ app/src/main/java/com/zharfan/androidfundamental/activity/OptionMenuActivity.kt
@@ -1,11 +1,15 @@
 package com.zharfan.androidfundamental.activity
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.zharfan.androidfundamental.R
 import com.zharfan.androidfundamental.databinding.ActivityOptionMenuBinding
 import com.zharfan.androidfundamental.fragment.MenuFragment
@@ -22,7 +26,28 @@ class OptionMenuActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.option_menu,menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as  SearchManager
+        val searchView = menu?.findItem(R.id.action_search)?.actionView as SearchView
+
+        searchView.apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            queryHint = resources.getString(R.string.search_hint)
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    Toast.makeText(this@OptionMenuActivity, query, Toast.LENGTH_SHORT).show()
+                    clearFocus()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+
+            })
+        }
         return true
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
