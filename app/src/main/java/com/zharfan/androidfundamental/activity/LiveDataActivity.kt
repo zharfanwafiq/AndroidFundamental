f764ab122f7a3167581621f8626277e03493ec98
zharfanwafiq <zharfanww@gmail.com>
2022-05-25 16:30:57 +0700

    update dependencies

---
+++ app/src/main/java/com/zharfan/androidfundamental/activity/LiveDataActivity.kt
@@ -0,0 +1,11 @@
+package com.zharfan.androidfundamental.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LiveDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live_data)
+    }
+}
